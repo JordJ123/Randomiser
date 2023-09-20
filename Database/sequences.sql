be6b841c60da9ed0fe65e7bb5e306d133cd3e6c7
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2023-09-20 01:23:06 +0100

    -

--- Database/sequences.sql
+++ Database/sequences.sql
@@ -1,13 +1,23 @@
 --------------------------------------------------------
---  File created - Saturday-August-12-2023   
+--  File created - Wednesday-September-20-2023   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Sequence LOCATIONS_AUTOINCREMENT
 --------------------------------------------------------
 
-   CREATE SEQUENCE  "SYSTEM"."LOCATIONS_AUTOINCREMENT"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 119 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+   CREATE SEQUENCE  "SYSTEM"."LOCATIONS_AUTOINCREMENT"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 519 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
 --------------------------------------------------------
 --  DDL for Sequence GAMES_AUTOINCREMENT
 --------------------------------------------------------
 
    CREATE SEQUENCE  "SYSTEM"."GAMES_AUTOINCREMENT"  MINVALUE 1 MAXVALUE 9999999999 INCREMENT BY 1 START WITH 3 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MOVIE_CATEGORIES_AUTOINCREMENT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."MOVIE_CATEGORIES_AUTOINCREMENT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 2 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
+--------------------------------------------------------
+--  DDL for Sequence MOVIES_AUTOINCREMENT
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "SYSTEM"."MOVIES_AUTOINCREMENT"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 69 NOCACHE  NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
